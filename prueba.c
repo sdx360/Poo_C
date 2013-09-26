1aceadc1c42710d1d573561c244f73fe833272c2
Dan Williams <cti201108@iMac-de-cti201108.local>
2013-09-25 19:47:28 -0500

    Hoja de calculo v0.3

--- prueba.c
+++ prueba.c
@@ -3,12 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 int main(int argc, char** argv) {
-float number1;
-    
-   printf("\nEnter any integer:");
-	scanf("%f", &number1);
+	char cadena[20];
+    char temp[8]="asdf";
+    //fgets(cadena,20,stdin);if(isdigit(temp)!=0){
+ 		float number1=atof(temp);
+ 		printf("%.2f", number1);
+ 	
+ 	
+ 	//int as=strlen(cadena);
+	
 	
        
     return 0;
